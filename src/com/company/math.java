cdb88f800f99c84e1f11b57b57e7a2cebc6dc157
malmP <pallemalm@gmail.com>
2019-12-09 11:08:50 +0100

    nuuddå

--- src/com/company/math.java
+++ src/com/company/math.java
@@ -4,9 +4,9 @@ import org.w3c.dom.ls.LSOutput;
 
 public class math {
 
-    int a = 5;
-    int b = 6;
-    int c = a + b;
+    int xxx = 5;
+    int yyyy = 6;
+    int BBB = a + b;
 
 
  int d = b-a;
